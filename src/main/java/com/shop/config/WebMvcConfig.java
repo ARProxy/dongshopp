09f7e04a9334ef1b21cf47cf62e0547f82429270
ARProxy <hocci0222@kakao.com>
2023-10-30 18:00:46 +0900

    Initial commit

---
+++ src/main/java/com/shop/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.shop.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Value("${uploadPath}")
+    String uploadPath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations(uploadPath);
+    }
+
+}
